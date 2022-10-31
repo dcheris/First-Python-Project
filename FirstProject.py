28b854ba27fcaabf8f7b0f39f11014e1fcc6489b
dcheris <dcheris@luc.edu>
2022-10-30 22:17:55 -0500

    Initial commit

---
+++ FirstProject.py
@@ -0,0 +1,28 @@
+# Simple addition
+print(5 + 2)
+
+# Power of 8
+print(4**8)
+
+# Remainder
+print(200 % 7)
+
+height = 1.79
+weight = 68.7
+bmi = height * weight
+type(bmi)
+print("Your BMI is: " + str(bmi))
+
+growth_multiplier = 1.1
+savings = 100
+
+result = growth_multiplier **7 * savings
+print(result)
+
+desc = "compound interest"
+profitable = True
+
+print(type(desc))
+
+print("I can add integers, like " + str(5) + " to strings.")
+

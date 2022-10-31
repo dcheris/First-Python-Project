bc825904bf66901d36fed16b8f399269231fe12f
dcheris <97857929+dcheris@users.noreply.github.com>
2022-10-30 23:34:56 -0500

    "changed email to no reply email"

--- FirstProject.py
+++ FirstProject.py
@@ -13,8 +13,8 @@ bmi = height * weight
 type(bmi)
 print("Your BMI is: " + str(bmi))
 
-growth_multiplier = 1.1
-savings = 100
+growth_multiplier = 1.9
+savings = 400
 
 result = growth_multiplier ** 8 * savings
 print(result)
